d17657064cb8ec0723c92089fd3948decaf87d54
doujinreviewers <doujin.reviewers@gmail.com>
2021-11-19 17:20:36 +0900

    plugincomment

--- webpack.config.plugin.js
+++ webpack.config.plugin.js
@@ -1,5 +1,6 @@
+const os = require("os")
+const fs = require('fs')
 const path = require('path')
-const webpack = require('webpack')
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
@@ -43,4 +44,16 @@ module.exports = {
       }
     ]
   },
+  plugins: [
+    new class AddPluginComments {
+      apply(compiler) {
+        compiler.hooks.afterEmit.tap('AddPluginComments', compilation => {
+          const { options: { output: { path: dir, filename } } } = compilation
+          const output_file_path = path.resolve(dir, filename)
+          const pc = path.resolve(__dirname, 'src', 'assets', 'plugincomment.txt')
+          fs.writeFileSync(output_file_path, fs.readFileSync(pc)+os.EOL+fs.readFileSync(output_file_path))
+        })
+      }
+    }()
+  ],
 }
